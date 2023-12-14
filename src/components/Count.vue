0238a351e396654342f40a228b24c6c9c5cd43e0
simonmcho <simonmcho@gmail.com>
2023-12-14 17:05:36 -0500

    Initial testing of vue

---
+++ src/components/Count.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      console: console,
+      rawHtml: '<span style="color: red">This should be red.</span>',
+      idKey: 'some-id',
+      seen: false,
+      toggleSeen: () => {
+        this.seen = !this.seen
+      } 
+    }
+  }
+}
+</script>
+
+<template>
+  <button @click="count++">Click me for count: {{  count  }}{{  console.log({ count }) }}</button>
+  <p>Using text interpolation: {{ rawHtml }}</p>
+  <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+  <div v-bind:id="idKey">HELLO</div>
+  <div>
+    <button @click="toggleSeen">Click me to see/hide element below</button>
+    <div v-if="seen">
+      SEEN
+    </div>
+  </div>
+</template>
